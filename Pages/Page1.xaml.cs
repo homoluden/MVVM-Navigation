7dd1655e0e5de6ed6932b4994d84a66ad029ca42
Alexey Matveev <alexey_matveev@epam.com>
2013-09-30 21:22:00 +0400

    Pages: code cleanup and Page404 addition

--- Pages/Page1.xaml.cs
+++ Pages/Page1.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ViewModels;
-
-namespace Pages
+﻿namespace Pages
 {
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class Page1 : BasePage
+    public partial class Page1
     {
         public Page1()
         {
